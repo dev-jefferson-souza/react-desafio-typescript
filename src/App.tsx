e36a2302bdfbac90f3670762e182cea1bb10c990
Jefferson Souza <jefferson.souza.correa@gmail.com>
2023-03-13 16:39:56 -0300

    feat(Login): Bolha animada

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import { Login } from "./pages/Login";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <Login />
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import { Blob, Container, Form } from "./styles";
+
+export const Login = () => {
+  return (
+    <Container>
+      <Blob />
+      <Form>
+        <h1>Login</h1>
+      </Form>
+    </Container>
+  );
+};
